6063cf24ea46a1a87d5766eed6ee6d9be85319dd
maddieiyengar <119278009+maddieiyengar@users.noreply.github.com>
2025-04-21 21:53:53 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+$host = '127.0.0.1';
+$db   = 'jctax';
+$user = 'root';
+$pass = '';  // or your own password
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    exit('Database connection failed: ' . $e->getMessage());
+}
+?>
